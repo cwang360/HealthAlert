61a7f5b3b5c87e1abdbc1bde51d7a9553c913255
Yu Pan <70486009+yu-pan-cpu@users.noreply.github.com>
2020-12-20 06:20:00 -0500

    Update main.dart

--- healthAlertFiles/main.dart
+++ healthAlertFiles/main.dart
@@ -6,8 +6,13 @@ import 'package:page_transition/page_transition.dart';
 import 'dart:async';
 import 'dart:isolate';
 
+import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:sms_maintained/sms.dart';
+
 //background packages
 //import 'package:shared_preferences/shared_preferences.dart';
+//import 'package:workmanager/workmanager.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 //geo packages
@@ -51,8 +56,29 @@ final AndroidGeofencingSettings androidSettings = AndroidGeofencingSettings(
 FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
     FlutterLocalNotificationsPlugin();
 
-//where WorkManager tasks are executed. Deprecated
 void callbackDispatcher() {
+  // Workmanager.executeTask((task, inputData) async {
+  //   switch (task) {
+  //     case reminder:
+  //       print("$reminder executed");
+  //       break;
+  //     case remind_later:
+  //       print("$remind_later executed");
+  //       break;
+  //     case remind_short:
+  //       print("$remind_short executed");
+  //       break;
+  //     case Workmanager.iOSBackgroundTask:
+  //       print("The iOS background fetch was triggered");
+  //       Directory tempDir = await getTemporaryDirectory();
+  //       String tempPath = tempDir.path;
+  //       print(
+  //           "You can access other plugins in the background, for example Directory.getTemporaryDirectory(): $tempPath");
+  //       break;
+}
+
+//where WorkManager tasks are executed. Deprecated
+// void callbackDispatcher() {
 //   Workmanager.executeTask((task, inputData) async {
 //     switch (task) {
 //       case reminder:
@@ -71,11 +97,11 @@ void callbackDispatcher() {
 //         print(
 //             "You can access other plugins in the background, for example Directory.getTemporaryDirectory(): $tempPath");
 //         break;
-    }
+//     }
 
-    return Future.value(true);
-  });
-}
+//     return Future.value(true);
+//   });
+// }
 
 class MyApp extends StatefulWidget {
   @override
@@ -236,11 +262,11 @@ class _MyHomePageState extends State<MyHomePage> {
 
                 //change this to ontap
                 onPressed: () {
-//                   Workmanager.initialize(
-//                     callbackDispatcher,
-//                     isInDebugMode: true,
-//                   );
-//                   print("WorkManager Online");
+                  // Workmanager.initialize(
+                  //   callbackDispatcher,
+                  //   isInDebugMode: true,
+                  // );
+                  // print("WorkManager Online");
                   Navigator.push(
                       context,
                       PageTransition(
@@ -549,8 +575,8 @@ class _ReminderPageState extends State<ReminderPage> {
   Future<void> scheduleNotification(int timeAway) async {
     var scheduledNotificationDateTime =
         //DateTime.now().add(Duration(hours: interval));
-        //DateTime.now().add(Duration(seconds: 5));
-        DateTime.now().add(Duration(minutes: timeAway));
+        DateTime.now().add(Duration(seconds: 5));
+    //DateTime.now().add(Duration(minutes: timeAway));
     var androidPlatformChannelSpecifics = AndroidNotificationDetails(
       'channel id',
       'channel name',
@@ -664,43 +690,43 @@ class NewScreen extends StatelessWidget {
   }
 }
 
-class StatusPage extends StatefulWidget {
-  @override
-  _StatusPageState createState() => _StatusPageState();
-}
+// class StatusPage extends StatefulWidget {
+//   @override
+//   _StatusPageState createState() => _StatusPageState();
+// }
 
-//This displays vitals associated with the wearable device
-class _StatusPageState extends State<StatusPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        body: Stack(
-      children: <Widget>[
-        Align(
-          alignment: Alignment(0.0, -0.8),
-          child: Text("Humidity here"),
-        ),
-        Align(
-          alignment: Alignment(0.0, -0.7),
-          child: Text("Temperature here"),
-        ),
-        Align(
-          alignment: Alignment(0.0, -0.6),
-          child: Text("Falling here"),
-        ),
-        Align(
-          alignment: Alignment(1, 1),
-          child: FlatButton(
-            child: Text("Back", style: TextStyle(color: Colors.blue)),
-            onPressed: () {
-              Navigator.pop(context);
-            },
-          ),
-        ),
-      ],
-    ));
-  }
-}
+// //This displays vitals associated with the wearable device
+// class _StatusPageState extends State<StatusPage> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//         body: Stack(
+//       children: <Widget>[
+//         Align(
+//           alignment: Alignment(0.0, -0.8),
+//           child: Text("Humidity here"),
+//         ),
+//         Align(
+//           alignment: Alignment(0.0, -0.7),
+//           child: Text("Temperature here"),
+//         ),
+//         Align(
+//           alignment: Alignment(0.0, -0.6),
+//           child: Text("Falling here"),
+//         ),
+//         Align(
+//           alignment: Alignment(1, 1),
+//           child: FlatButton(
+//             child: Text("Back", style: TextStyle(color: Colors.blue)),
+//             onPressed: () {
+//               Navigator.pop(context);
+//             },
+//           ),
+//         ),
+//       ],
+//     ));
+//   }
+// }
 
 class GeoReminders extends StatefulWidget {
   @override
@@ -1016,5 +1042,364 @@ class _GeoInputPageState extends State<GeoInputPage> {
         });
       }
     });
+  }
+}
+
+String recipientNum = "";
+bool sent = false;
+
+class StatusPage extends StatefulWidget {
+  StatusPage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _StatusPageState createState() => _StatusPageState();
+}
+
+class _StatusPageState extends State<StatusPage> {
+  final FlutterReactiveBle _ble = FlutterReactiveBle();
+  StreamSubscription _subscription;
+  StreamSubscription<ConnectionStateUpdate> _connection;
+  int temperature;
+  String temperatureStr = "Hello";
+  String humidityStr = "Hola";
+  String imuStr = "Welcome";
+
+  void _disconnect() async {
+    _subscription?.cancel();
+    if (_connection != null) {
+      await _connection.cancel();
+    }
+  }
+
+  void _connectBLE() {
+    setState(() {
+      temperatureStr = 'Loading';
+      humidityStr = 'Loading';
+      imuStr = 'Loading';
+    });
+    _disconnect();
+    _subscription = _ble.scanForDevices(
+        withServices: [],
+        scanMode: ScanMode.lowLatency,
+        requireLocationServicesEnabled: true).listen((device) {
+      if (device.name == 'Nano33BLESENSE') {
+        print('Nano33BLESENSE found!');
+        _connection = _ble
+            .connectToDevice(
+          id: device.id,
+        )
+            .listen((connectionState) async {
+          // Handle connection state updates
+          print('connection state:');
+          print(connectionState.connectionState);
+          if (connectionState.connectionState ==
+              DeviceConnectionState.connected) {
+            // final characteristic1 = QualifiedCharacteristic(
+            //     serviceId: Uuid.parse("181A"),
+            //     characteristicId: Uuid.parse("2A6E"),
+            //     deviceId: device.id);
+            // final response1 = await _ble.readCharacteristic(characteristic1);
+            // print(response1);
+
+            // final characteristic2 = QualifiedCharacteristic(
+            //     serviceId: Uuid.parse("281A"),
+            //     characteristicId: Uuid.parse("4A6E"),
+            //     deviceId: device.id);
+            // final response2 = await _ble.readCharacteristic(characteristic2);
+            // print(response2);
+            final tempChar = QualifiedCharacteristic(
+                serviceId: Uuid.parse("181A"),
+                characteristicId: Uuid.parse("2A6E"),
+                deviceId: device.id);
+            _ble.subscribeToCharacteristic(tempChar).listen((data) {
+              print(data);
+              final tempResponse = data;
+              setState(() {
+                temperature = tempResponse[0];
+                temperatureStr =
+                    'Temperature: ' + temperature.toString() + '°C';
+              });
+            }, onError: (dynamic error) {
+              print("error updating temp status");
+            });
+
+            final humidityChar = QualifiedCharacteristic(
+                serviceId: Uuid.parse("181A"),
+                characteristicId: Uuid.parse("2A6F"),
+                deviceId: device.id);
+            _ble.subscribeToCharacteristic(humidityChar).listen((data) {
+              print(data);
+              final humidityResponse = data;
+              setState(() {
+                humidityStr =
+                    'Humidity: ' + humidityResponse[0].toString() + '%';
+              });
+            }, onError: (dynamic error) {
+              print("error updating humidity status");
+            });
+
+            final dangerChar = QualifiedCharacteristic(
+                serviceId: Uuid.parse("281A"),
+                characteristicId: Uuid.parse("4A6E"),
+                deviceId: device.id);
+            _ble.subscribeToCharacteristic(dangerChar).listen((data) {
+              print(data);
+              var dangerResponse;
+              if (data[0] == 1) {
+                dangerResponse =
+                    "Danger - User has fallen and has not moved for more than 10 seconds";
+                if (!sent) {
+                  SmsSender sender = new SmsSender();
+                  String address = recipientNum;
+                  String msg =
+                      "From HealthAlert: I have fallen and have not moved for more than 10 seconds.";
+                  print(address);
+                  print(msg);
+                  SmsMessage message = new SmsMessage(address, msg);
+                  message.onStateChanged.listen((state) {
+                    if (state == SmsMessageState.Sent) {
+                      print("SMS is sent!");
+                    } else if (state == SmsMessageState.Delivered) {
+                      print("SMS is delivered!");
+                    }
+                  });
+                  sender.sendSms(message);
+                  sent = true;
+                }
+              } else {
+                dangerResponse = "Normal";
+                sent = false;
+              }
+              setState(() {
+                imuStr = 'Activity: ' + dangerResponse;
+              });
+            }, onError: (dynamic error) {
+              print("error updating fall status");
+            });
+
+            final tremorChar = QualifiedCharacteristic(
+                serviceId: Uuid.parse("281A"),
+                characteristicId: Uuid.parse("4A6F"),
+                deviceId: device.id);
+            _ble.subscribeToCharacteristic(tremorChar).listen((data) {
+              print(data);
+              var tremorResponse;
+              if (data[0] == 1) {
+                tremorResponse =
+                    "Danger - User is experiencing severe tremors that may indicate a seizure";
+                if (!sent) {
+                  SmsSender sender = new SmsSender();
+                  String address = recipientNum;
+                  String msg =
+                      "From HealthAlert: I am experiencing severe tremors that may indicate a seizure.";
+                  print(address);
+                  print(msg);
+                  SmsMessage message = new SmsMessage(address, msg);
+                  message.onStateChanged.listen((state) {
+                    if (state == SmsMessageState.Sent) {
+                      print("SMS is sent!");
+                    } else if (state == SmsMessageState.Delivered) {
+                      print("SMS is delivered!");
+                    }
+                  });
+                  sender.sendSms(message);
+                  sent = true;
+                }
+              } else {
+                tremorResponse = "Normal";
+                sent = false;
+              }
+              setState(() {
+                imuStr = 'Activity: ' + tremorResponse;
+              });
+            }, onError: (dynamic error) {
+              print("error updating tremor status");
+            });
+
+            // setState(() {
+            //   temperature = response1[0];
+            //   temperatureStr = temperature.toString() + '°' ;
+            // });
+            // _disconnect();
+            // print('disconnected');
+          }
+        }, onError: (dynamic error) {
+          // Handle a possible error
+          print(error.toString());
+        });
+      }
+    }, onError: (error) {
+      print('error!');
+      print(error.toString());
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                      begin: Alignment.topRight,
+                      end: Alignment.bottomLeft,
+                      colors: [
+                const Color(0xff3498eb),
+                const Color(0xffc55dfc)
+              ]))),
+          Align(
+              alignment: Alignment(0.0, -0.7),
+              child: Text(
+                temperatureStr,
+                style: GoogleFonts.poppins(
+                    textStyle: Theme.of(context)
+                        .textTheme
+                        .headline5
+                        .copyWith(color: Colors.white)),
+              )),
+          Align(
+            alignment: Alignment(0.0, -0.5),
+            child: Text(
+              humidityStr,
+              style: GoogleFonts.poppins(
+                  textStyle: Theme.of(context)
+                      .textTheme
+                      .headline5
+                      .copyWith(color: Colors.white)),
+            ),
+          ),
+          Align(
+            alignment: Alignment(0.0, -0.3),
+            child: Text(
+              imuStr,
+              style: GoogleFonts.poppins(
+                  textStyle: Theme.of(context)
+                      .textTheme
+                      .headline5
+                      .copyWith(color: Colors.white)),
+            ),
+          ),
+          Align(
+            alignment: Alignment(0.0, 0.8),
+            child: FlatButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Settings()),
+                );
+              },
+              child: Text(
+                "Settings",
+                style: GoogleFonts.poppins(
+                    textStyle: Theme.of(context)
+                        .textTheme
+                        .headline5
+                        .copyWith(color: Colors.white)),
+              ),
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _connectBLE,
+        tooltip: 'Increment',
+        backgroundColor: Color(0xFF74A4BC),
+        child: Icon(Icons.loop),
+      ),
+    );
+  }
+}
+
+class Settings extends StatefulWidget {
+  @override
+  _Settings createState() => _Settings();
+}
+
+class _Settings extends State<Settings> {
+  final myController = TextEditingController();
+
+  @override
+  void dispose() {
+    myController.dispose();
+    super.dispose();
+  }
+
+  String input;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    begin: Alignment.topRight,
+                    end: Alignment.bottomLeft,
+                    colors: [
+                  const Color(0xff3498eb),
+                  const Color(0xffc55dfc)
+                ])),
+          ),
+          Align(
+              alignment: Alignment(-0.5, -0.7),
+              child: Text('Enter the phone number to send alerts to: ',
+                  style: GoogleFonts.poppins(
+                      textStyle: Theme.of(context)
+                          .textTheme
+                          .bodyText1
+                          .copyWith(color: Colors.white)))),
+          Align(
+            alignment: Alignment(-0.5, -0.5),
+            child: Padding(
+              padding: EdgeInsets.all(20.0),
+              child: TextFormField(
+                controller: myController,
+                decoration: InputDecoration(
+                  labelText: "Current Recipient: " + recipientNum,
+                  labelStyle: TextStyle(
+                      color: Colors.white, fontFamily: "Poppins", fontSize: 20),
+                ),
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment(0.0, -0.1),
+            child: FlatButton(
+              onPressed: () {
+                recipientNum = myController.text;
+                print(recipientNum);
+              },
+              child: Text(
+                "Save",
+                style: GoogleFonts.poppins(
+                    textStyle: Theme.of(context)
+                        .textTheme
+                        .headline5
+                        .copyWith(color: Colors.white)),
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment(1.0, 1.0),
+            child: FlatButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text(
+                "Back",
+                style: GoogleFonts.poppins(
+                    textStyle: Theme.of(context)
+                        .textTheme
+                        .headline5
+                        .copyWith(color: Colors.white)),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
